66b9cb9acc7bb5fa51cba7678d086054f1bd4a3f
Luca Capra <luca.capra@eai.eu>
2014-05-15 15:07:16 +0200

    added test for parallel data push

--- test/spec/so.spec.js
+++ test/spec/so.spec.js
@@ -1,6 +1,6 @@
 describe('ServiceObject', function() {
 
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
 
     var compose = require("../../index");
 
@@ -18,16 +18,16 @@ describe('ServiceObject', function() {
     var smartphone = null;
     var smartphoneDefinition = require('./smartphone.so').definition;
 
-    it('List SO', function(done) {
-        compose.list()
-            .then(function(list) {
-                expect(list.length > 0).toBeTruthy();
-            })
-            .catch(console.log)
-            .finally(done);
-    });
-
-
+//    it('List SO', function(done) {
+//        compose.list()
+//            .then(function(list) {
+//                expect(list.length > 0).toBeTruthy();
+//            })
+//            .catch(console.log)
+//            .finally(done);
+//    });
+//
+//
     it('Create SO', function(done) {
         compose.create(smartphoneDefinition)
             .then(function(so) {
@@ -37,27 +37,27 @@ describe('ServiceObject', function() {
             .catch(console.log)
             .finally(done);
     });
-
-    it('Load SO', function(done) {
-        compose.load(smartphone.id)
-            .then(function(so) {
-                expect(so.id).toEqual(smartphone.id);
-            })
-            .catch(console.log)
-            .finally(done);
-    });
-
-    it('Update SO custom fields', function(done) {
-        var time = new Date().getTime();
-        smartphone.customFields.newTestField = time;
-        smartphone.update()
-            .then(function(so) {
-                expect(smartphone.customFields.newTestField).toEqual(time);
-            })
-            .catch(console.log)
-            .finally(done);
-    });
-
+//
+//    it('Load SO', function(done) {
+//        compose.load(smartphone.id)
+//            .then(function(so) {
+//                expect(so.id).toEqual(smartphone.id);
+//            })
+//            .catch(console.log)
+//            .finally(done);
+//    });
+//
+//    it('Update SO custom fields', function(done) {
+//        var time = new Date().getTime();
+//        smartphone.customFields.newTestField = time;
+//        smartphone.update()
+//            .then(function(so) {
+//                expect(smartphone.customFields.newTestField).toEqual(time);
+//            })
+//            .catch(console.log)
+//            .finally(done);
+//    });
+//
     it('Push and pull stream data', function(done) {
 
         var stream = smartphone.getStream('location');
@@ -89,20 +89,68 @@ describe('ServiceObject', function() {
                         .catch(console.log)
                         .finally(done);
 
-                }, 1500);
+                }, 2000);
 
             })
-            .catch(console.log)
+            .catch(console.log);
 
     });
 
-    it('Delete SO', function(done) {
-        smartphone.delete()
-            .then(function(so) {
-                expect(so.id).toEqual(null);
-            })
-            .catch(console.log)
-            .finally(done);
+    it('Push a bunch parallel requests', function(done) {
+
+        var stream = smartphone.getStream('location');
+        var getData = function(i) {
+            return {
+                channels: {
+                    latitude:  11 + Math.random(),
+                    longitude: 45 + Math.random()
+                },
+                lastUpdate: (new Date).getTime() + (i * 2000)
+            };
+        };
+
+        var amount = 50;
+        var counter = 0;
+        for(var i = 0; i < amount; i++) {
+
+            var data = getData(i);
+            stream.push(data.channels, data.lastUpdate)
+                .then(function() {
+                    counter++;
+                })
+                .catch(console.log);
+        }
+
+        var intv = setInterval(function() {
+
+            if(counter !== amount) return;
+
+            clearInterval(intv);
+
+            setTimeout(function() {
+                stream.pull().then(function(data) {
+
+//                    console.log(data);
+//                    console.log(smartphone.id);
+
+                    expect(data.size()).toEqual(amount + 1);
+                })
+                .catch(console.log)
+                .finally(done);
+            }, 10000);
+
+
+        }, 100);
+
     });
+
+//    it('Delete SO', function(done) {
+//        smartphone.delete()
+//            .then(function(so) {
+//                expect(so.id).toEqual(null);
+//            })
+//            .catch(console.log)
+//            .finally(done);
+//    });
 
 });
